6f556cc240ef5bd399a037b7c02f1b81fe431ae1
kanchan0711 <kanchan08642@gmail.com>
2025-05-10 22:39:06 +0530

    setup Tailwind and firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl text-center'> hey</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAAV9FPsO8gE7dNySvpXZaTQWGoY98Pwc",
+  authDomain: "file-management-system-16240.firebaseapp.com",
+  projectId: "file-management-system-16240",
+  storageBucket: "file-management-system-16240.firebasestorage.app",
+  messagingSenderId: "96092558835",
+  appId: "1:96092558835:web:0d2f9da084a60322fb2e86",
+  measurementId: "G-C8NKTTZXL3"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
